f47e35f8bc48db7057eafbb28b59941f3ad99b56
Konadu <cunaedu@gmail.com>
2022-05-17 16:12:00 +0000

    added random meme images

--- src/Common/MemeForm/MemeForm.jsx
+++ src/Common/MemeForm/MemeForm.jsx
@@ -1,8 +1,21 @@
 import "./MemeForm.css"
+import memesData from "./memeData"
+import { useState } from "react"
+
 export default function MemeForm(){
+     const [memeImage , setMemeImage] = useState("")
+    const getMeme = () =>{
+        const memes = memesData.data.memes
+        const randomElement = Math.floor(Math.random() * memes.length)
+        const meme = memes[randomElement]
+        console.log(meme)
+        setMemeImage(meme.url)
+        return memeImage
+     }
+
     return(
         <section>
-            <form className="meme-form">
+            <div className="meme-form">
                 <div className="form-control">
                     <input type= "text" placeholder="Top Text"/>
                 </div>
@@ -10,9 +23,17 @@ export default function MemeForm(){
                     <input type="text" placeholder="Bottom Text"/>
                 </div>
                 <div className="form-submit">
-                    <button className="submit-button submit"> Generate Meme</button>
+                    <button 
+                        className="submit-button submit"
+                        onClick={getMeme}
+                        >
+                        Generate Meme
+                    </button>
                 </div>
-            </form>
+            </div>
+            <div className="meme">
+                <img className="memeImage" src={memeImage} alt="" />
+            </div>
         </section>
     )
 }
